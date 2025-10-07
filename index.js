bf91247bd0028274ed628af370f8e18b821961f0
RvLionXz <rvlionxz@gmail.com>
2025-10-07 14:03:07 +0700

    edit index.js

--- index.js
+++ index.js
@@ -7,35 +7,38 @@ const userRoutes = require("./routes/userRoutes");
 const absensiRoutes = require("./routes/absensiRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-app.use(
-	cors({
-		origin: "*",
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		preflightContinue: false,
-		optionsSuccessStatus: 204,
-		allowedHeaders: "Content-Type, Authorization",
-	})
-);
+const whitelist = [
+	"https://<nama-proyek-anda>.vercel.app",
+	"http://localhost:5173",
+];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
+// =================================================================
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("API Absensi Klinik Berjalan!");
 });
 
-app.options('*', cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/absensi", absensiRoutes);
 
-app.use((err, req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	next(err);
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Server berjalan di http://localhost:${PORT}`);
 });
-
-// app.listen(PORT, () => {
-// 	console.log(`Server berjalan di http://localhost:${PORT}`);
-// });
-
-module.exports = app;
